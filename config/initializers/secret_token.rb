684161e859c54703341e19837b295864a25de7ba
Robert Cotey <coteyr@coteyr.net>
2013-10-11 18:32:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Modsfortowns::Application.config.secret_key_base = '0eccdf58b5d2d55f7306cd3102c469164de67483a57372daa8d9f2986c796ea86302c6dc3cbb9708a5a7c6bc0709e9c634841c4405025ac6c3e3a9ed254e66ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Modsfortowns::Application.config.session_store :cookie_store, key: '_modsfortowns_session'
